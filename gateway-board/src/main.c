0764eead9d93bb9581deae49e2a7be8f0d05e740
Adrian Suliga <adriansuliga81@gmail.com>
2026-05-02 16:44:34 +0200

    [GATEWAY] Fragment and send hard-coded user application

--- gateway-board/src/main.c
+++ gateway-board/src/main.c
@@ -3,7 +3,9 @@
 #include <zephyr/drivers/can.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(Gateway, LOG_LEVEL_DBG);
+#include "user-application.h"
+
+LOG_MODULE_REGISTER(gateway, LOG_LEVEL_DBG);
 
 #define CAN_SEND_INTERVAL_MS 100
 
@@ -63,21 +65,62 @@ int main()
         return 1;
     }
 
-    while (true) {
-        struct can_frame frame = {
-            .id = 0x1,
-            .flags = 0,
-            .dlc = 2,
-            .data = {0xAB, 0xCD}
-        };
+    LOG_INF("Start sending app");
 
-        ret = can_send(can_dev, &frame, K_SECONDS(1), &tx_callback, NULL);
+    int idx = 0;
+    const int can_payload_size = 8;
+    while (idx + can_payload_size <= user_application_bin_len) {
+        struct can_frame frame = {
+            .id = 0x10, // arbitrary chosen
+            .flags = 0,
+            .dlc = can_payload_size,
+        };
+        memcpy(frame.data, user_application_bin + idx, can_payload_size);
+
+        ret = can_send(can_dev, &frame, K_FOREVER, &tx_callback, NULL);
         if (ret < 0) {
             LOG_ERR("Failed to send CAN frame, error %d", ret);
+            return 1;
         } else {
-            LOG_INF("Frame %d sent", frame.id);
+            LOG_INF("Sent (%d / %d) [ %02x %02x %02x %02x %02x %02x %02x %02x ]",
+                    idx + 8, user_application_bin_len,
+                    frame.data[0], frame.data[1], frame.data[2],
+                    frame.data[3], frame.data[4], frame.data[5],
+                    frame.data[6], frame.data[7]);
         }
 
+        idx += can_payload_size;
+    }
+
+    struct can_frame frame = {
+        .id = 0x10,
+        .flags = 0,
+        .dlc = user_application_bin_len - idx
+    };
+    for (int i = 0; i < frame.dlc; ++i) {
+        frame.data[i] = user_application_bin[idx + i];
+    }
+
+    ret = can_send(can_dev, &frame, K_FOREVER, &tx_callback, NULL);
+    if (ret < 0) {
+        LOG_ERR("Failed to send CAN frame, error %d", ret);
+        return 1;
+    } else {
+        LOG_INF("Sent (%d / %d)", user_application_bin_len, user_application_bin_len);
+    }
+
+    frame.id = 0x11;
+    ret = can_send(can_dev, &frame, K_FOREVER, &tx_callback, NULL);
+    if (ret < 0) {
+        LOG_ERR("Failed to send CAN frame, error %d", ret);
+        return 1;
+    } else {
+        LOG_INF("Sent EOT frame");
+    }
+
+    LOG_INF("Flashing successful\nGoing idle...");
+
+    while (true) {
         gpio_pin_toggle_dt(&led);
         k_msleep(CAN_SEND_INTERVAL_MS);
     }
